910f726bef2eed38f95a2e48bbc12323f91c4329
Vicfred <vicfred@vicfred.dev>
2020-04-17 02:44:47 -0500

    command line options and logger

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+#![forbid(unsafe_code)]
+
+use clap;
+use env_logger;
+use irc_bot;
+use log;
+
+use irc_bot::modules;
+use clap::{arg_enum, value_t};
+
 fn main() {
-    println!("Hello, world!");
+    let args = clap::App::new("egbot")
+        .arg(
+            clap::Arg::with_name("config-file")
+                .long("config-file")
+                .short("c")
+                .default_value("config.yaml"),
+        )
+        .arg(
+            clap::Arg::with_name("data-dir")
+                .long("data-dir")
+                .short("d")
+                .default_value("data"),
+        )
+        .arg(
+            clap::Arg::with_name("error-verbosity")
+                .long("error-verbosity")
+                .possible_values(&ErrorVerbosity::variants())
+                .case_insensitive(true)
+                .default_value("Display"),
+        )
+        .get_matches();
+
+    env_logger::init();
+
+    let error_verbosity =
+        value_t!(args, "error-verbosity", ErrorVerbosity).unwrap_or_else(|err| err.exit());
 }
+
+arg_enum! {
+    #[derive(Debug)]
+    enum ErrorVerbosity {
+        Display,
+        Debug
+    }
+}
+
